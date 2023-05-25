cdf74f251881bc5042cce29ba071d44a13fdd4ae
CrSjimo <crs_16423@outlook.com>
2023-05-26 00:06:48 +0800

    add ui

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -8,7 +8,28 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     juce::ignoreUnused (processorRef);
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    mainButton.setButtonText("Show DiffScope Editor");
+    mainButton.setColour(juce::TextButton::buttonColourId, juce::Colour(0x15, 0x4c, 0x39));
+    mainButton.setColour(juce::TextButton::buttonOnColourId, juce::Colour(0x0f, 0x69, 0x4b));
+    mainButton.addListener(this);
+    errorLabel.setColour(juce::Label::textColourId, juce::Colours::red);
+    setStatus("Not Connected");
+//    setError("This is a test");
+    versionLabel.setText(juce::String("Version ") + JucePlugin_VersionString, juce::dontSendNotification);
+    versionLabel.setJustificationType(juce::Justification::right);
+    addAndMakeVisible(mainButton);
+    addAndMakeVisible(statusLabel);
+    addAndMakeVisible(errorLabel);
+    addAndMakeVisible(versionLabel);
+    setSize (400, 200);
+}
+
+void AudioPluginAudioProcessorEditor::setStatus(const juce::String &status) {
+    statusLabel.setText(juce::String("Status: ") + status, juce::dontSendNotification);
+}
+
+void AudioPluginAudioProcessorEditor::setError(const juce::String &error) {
+    errorLabel.setText(error, juce::dontSendNotification);
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -16,17 +37,23 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 }
 
 //==============================================================================
-void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
-{
+void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g) {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll(juce::Colour(0x1e, 0x1e, 0x1e));
+    g.setColour(juce::Colour(0x25, 0x25, 0x26));
+    g.fillRect(0, 150, 400, 50);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    mainButton.setBounds(100, 50, 200, 50);
+    statusLabel.setBounds(8, 176, 384, 16);
+    errorLabel.setBounds(8, 160, 384, 16);
+    versionLabel.setBounds(8, 176, 384, 16);
+}
+
+void AudioPluginAudioProcessorEditor::buttonClicked(juce::Button *btn) {
+
 }
--- PluginEditor.h
+++ PluginEditor.h
@@ -3,7 +3,7 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor, public juce::Button::Listener
 {
 public:
     explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
@@ -12,12 +12,17 @@ public:
     //==============================================================================
     void paint (juce::Graphics&) override;
     void resized() override;
+    void buttonClicked(juce::Button *) override;
+    void setStatus(const juce::String &status);
+    void setError(const juce::String &error);
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
     juce::TextButton mainButton;
-
+    juce::Label statusLabel;
+    juce::Label errorLabel;
+    juce::Label versionLabel;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
